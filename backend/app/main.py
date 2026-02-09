96c94eceea2b23336adce776cfb2f5e8b12e275f
arilindra21 <141698279+arilindra21@users.noreply.github.com>
2026-02-09 22:18:34 +0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -90,6 +90,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=int(os.environ.get("PORT", 8000)),
         reload=settings.DEBUG,
     )
